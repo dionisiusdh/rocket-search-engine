a358d6d1b8dec0f816092006450c05c1180e0c89
WilsonTandya <wilson.tandya@gmail.com>
2020-10-31 12:27:45 +0700

    Home.css

--- src/client/src/modules/Home.js
+++ src/client/src/modules/Home.js
@@ -1,30 +1,40 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect} from 'react';
 import { Link } from 'react-router-dom';
 import { setCookie } from './../services/Cookie';
 import { Header, Container } from "semantic-ui-react";
+
+const divStyle = {
+    backgroundColor:'cyan',
+    height: '100%',
+    width: '100%'
+}
 
 function Home() {
     const [query, setQuery] = useState("");
 
     // Cookie
     setCookie('query',query,1);
-    
     return (
-        <div className="App">
-            <Container style={{marginTop:40, marginLeft:30, marginRight:30}}>
-                <Link to="/upload">Upload dokumen</Link>
-                <Header>Query</Header>
-                <form action="/post" method="post">
-                    <p>
-                        <input required type="text" name="que"
-                                placeholder="Query..." 
-                               onChange={e => setQuery(e.target.value)}/>
-                    </p>
-                    <Link to={"/result/" + query}><p><input type="submit" value="submit"/></p></Link>
-                </form>
-            </Container>
-        </div>
-    );
+        <body style={divStyle}>
+            <div className="App">
+                <Container style={{marginTop:40, marginLeft:30, marginRight:30, marginBottom:487}}>
+                    <Link to="/upload">Upload dokumen</Link>
+                    <Header>Query</Header>
+                    <form action="/post" method="post">
+                        <p>
+                            <input required type="text" name="que"
+                                    placeholder="Query..." 
+                                onChange={e => setQuery(e.target.value)}/>
+                        </p>
+                        <Link to={"/result/" + query}><p><input type="submit" value="submit"/></p></Link>
+                    </form>
+                </Container>
+            </div>
+            <footer className="footer">
+                <p>© 2020 IniGugel</p>
+            </footer>
+        </body>
+    )
 }
 
 export default Home;
